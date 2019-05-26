fd1e48bd72c150c2f61555957da8e83e2e183334
Amy Cen <amy.cen@blackrock.com>
2019-05-26 01:09:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,8 @@ require 'pry'
 class Dog
   attr_accessor :id, :name, :breed
 
-  def initialize(attributes)
-    def initialize(attributes)
-      attributes.each {|key, value| self.send(("#{key}="), value)}
-    end
+  def initialize(id: nil, name:, breed:)
+    @id, @name, @breed = id, name, breed
   end
 
   def self.create_table

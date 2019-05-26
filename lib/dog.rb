db74130bf0e5fed30d80841ad0065d8d0e97cac1
Amy Cen <amy.cen@blackrock.com>
2019-05-26 00:56:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ require 'pry'
 class Dog
   attr_accessor :id, :name, :breed
 
-  def initialize(id=nil, name=nil, breed=nil)
+  def initialize(id=nil, name, breed)
     @id, @name, @breed = id, name, breed
   end
 

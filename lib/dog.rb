093e47c6359d3e4661fb6ae6cb4b6793a28d9c04
Amy Cen <amy.cen@blackrock.com>
2019-05-26 01:36:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,10 +25,8 @@ class Dog
     DB[:conn].execute(sql)
   end
 
-  def self.new_from_db(rows)
-    rows.map do |row|
-      Dog.new(id: row[0], name: row[1], breed: row[2])
-    end
+  def self.new_from_db(row)
+    Dog.new(id: row[0], name: row[1], breed: row[2])
   end
 
   def self.find_by_name(name:)
